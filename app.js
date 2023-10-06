805c162668c1ae4b829daa0d4c58906a6140d0a7
syedwaleedali20 <syedwaleed887@gmail.com>
2023-10-05 17:56:41 -0700

    now ready to commit

---
+++ app.js
@@ -0,0 +1,156 @@
+// function popUp(){
+//     alert("hello friends")
+// }
+// popUp();
+
+//  function popUp(){
+//      alert("hello friends")}
+// for(var i = 0; i <= 6; i++) {
+//     popUp();
+// }
+
+// function greet(){
+//     var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+//  var fullName = firstName + " " + lastName;
+// console.log(fullName);
+// }
+// greet();
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(11, 10);
+
+
+
+// Calc // 
+// function calc (num1,opr,num2){
+// if(opr === "+"){
+//     return num1 + num2
+// }
+// else if (opr === "-"){
+//     return num1 - num2
+// }
+// else if (opr === "*"){
+//     return num1 * num2;
+// }
+// else {
+//     return "Incorrect operator:"
+// }
+// }
+// var result = calc(3,"+",66);
+// var result1 = calc(11,"-",7);
+// var result2 = calc(3,"*",8);
+// console.log(result)
+// console.log(result1);
+// console.log(result2);
+
+
+
+//   //
+// var arr = ["monday"," tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+// switch (arr) {
+// case "monday":
+// alert("today is monday");
+// break; 
+
+// case "tuesday":
+// alert("today is tuesday");
+// break;
+
+// case "wednesday":
+// alert("today is wednesday");
+// break;
+
+// case "thursday":
+// alert("today is thursday");
+// break;
+
+// case "friday":
+// alert("today is friday");
+// break;
+
+// case "saturday":
+// alert("today is saturday");
+// break;
+
+
+// case "sunday":
+// alert("today is sunday");
+// break;
+// }
+
+
+
+// 
+// function********
+// Q1
+// function greet() {
+//     var a = new Date();
+//     alert(a)
+// }
+// greet();
+
+//Q2
+// function greet() {
+//     var firstName = prompt("Enter Your firstname");
+//     var lastName = prompt("Enter Your lastname");
+//     var fullName = firstName + " " + lastName;
+//     alert(fullName);
+// }
+// greet();
+
+//Q3
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+// var num1 = +prompt("Enter First value");
+// var num2 = +prompt("Enter Second value");
+// var result = sum(num1, num2);
+// console.log(result);
+
+//Q4
+// //
+// function calce(num1, opr, num2) {
+//     if (opr === "+") {
+//         return num1 + num2;
+//     } else if (opr === "-") {
+//         return num1 - num2;
+//     } else if (opr === "*") {
+//         return num1 * num2;
+//     } else if (opr === "/") {
+//         return num1 / num2;
+//     }
+// }
+// var num1 = +prompt("Enter the value..");
+// var operator = prompt("Enter operator value..");
+// var num2 = +prompt("Enter the value..");
+
+// var result = calce(num1, operator, num2);
+// console.log(result);
+
+
+// //Q5
+// function squarNumber(number) {
+//     return number * number;
+// }
+// var inputNumber = prompt("Enter the value");
+// var result = squarNumber(inputNumber);
+// console.log("The square of" + (inputNumber) + "is" + (result));
+
+// //Q7
+// function displayCounting(start, end) {
+//     if (start <= end) {
+//         for (let i = start; i <= end; i++) {
+//             console.log(i);
+//         }
+//     } else {
+//         console.log("starting number must be less then or eqaul to the ending number")
+//     }
+// }
+
+// var startingNumber = Number(prompt("Enter the first number of Counting"));
+// var endingNumber = Number(prompt("Enter the last number of Counting"));
+// displayCounting(startingNumber, endingNumber);
